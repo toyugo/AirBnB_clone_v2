7686f737eb93856f0ce0db2f02ac270e9bf33e44
Toyugo <olivier.guyot@holbertonschool.com>
2021-03-29 19:52:25 +0000

    fix alchemy cause create user not best way

--- models/base_model.py
+++ models/base_model.py
@@ -51,6 +51,8 @@ class BaseModel:
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
+        if '_sa_instance_state' in dictionary:
+            del dictionary['_sa_instance_state']
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
