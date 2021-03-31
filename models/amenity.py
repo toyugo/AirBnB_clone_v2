2dc25619c5fadcfb0b77bf92d188bc39037796c7
O <olivier.guyot@holbertonschool.fr>
2021-03-31 12:19:54 +0000

    amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -2,7 +2,14 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import models
 
 class Amenity(BaseModel, Base):
-    __tablename__ = "amenities"    
-    name = ""
+    if models.storage_type == 'db':
+        __tablename__ = "amenities"    
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity",
+                                    viewonly=False)
+    else:
+        name = ""
