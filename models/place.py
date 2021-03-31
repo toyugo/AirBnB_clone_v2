f0bf909efb681c0fe531fb7568e62b61839f8cf1
O <olivier.guyot@holbertonschool.fr>
2021-03-31 13:16:33 +0000

    fix db

--- models/place.py
+++ models/place.py
@@ -10,21 +10,21 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey
 from sqlalchemy import Table
 
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id',
-                             String(60),
-                             ForeignKey('places.id'),
-                             primary_key=True,
-                             nullable=False),
-                      Column('amenity_id',
-                             String(60),
-                             ForeignKey('amenities.id'),
-                             primary_key=True,
-                             nullable=False))
-
 
 class Place(BaseModel, Base):
     """ A place to stay """
+    if models.storage_type == 'db':
+        place_amenity = Table('place_amenity', Base.metadata,
+                              Column('place_id',
+                                     String(60),
+                                     ForeignKey('places.id'),
+                                     primary_key=True,
+                                     nullable=False),
+                              Column('amenity_id',
+                                     String(60),
+                                     ForeignKey('amenities.id'),
+                                     primary_key=True,
+                                     nullable=False))
     amenity_ids = []
     if models.storage_type == 'db':
         __tablename__ = 'places'
