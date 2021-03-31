0b9df705b2031f60429f295e0244856a48123971
O <olivier.guyot@holbertonschool.fr>
2021-03-31 12:41:15 +0000

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -11,7 +11,6 @@ class Amenity(BaseModel, Base):
         __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
         place_amenities = relationship("Place",
-                                       secondary="place_amenity",
-                                       viewonly=False)
+                                       secondary="place_amenity")
     else:
         name = ""
