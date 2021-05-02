3e44da6794dd48e12eed8f8968acfd5ee9aa4cd0
Toyugo <2490@holbertonschool.com>
2021-05-02 14:34:51 +0000

    fix

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,20 +6,25 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+
+@app.route('/', strict_slashes=False)
 def index():
+    """ comment """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """ comment """
     return 'HBNB'
 
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes=False)
 def c(text):
+    """ comment """
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
 if __name__ == '__main__':
+    """ comment """
     app.run(host='0.0.0.0', debug=False)
