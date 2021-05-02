85bef3943bf3d07c702ad1e8869719487531c5ad
Toyugo <2490@holbertonschool.com>
2021-05-02 14:42:33 +0000

    fix

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,34 +1,49 @@
+#!/usr/bin/python3
+"""
+    setup 3
+"""
 from flask import Flask
-from flask import render_template
-
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', strict_slashes=False)
 def index():
+    """ comment """
     return 'Hello HBNB!'
 
-@app.route('/hbnb')
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """ comment """
     return 'HBNB'
 
-@app.route('/c/<text>')
+
+@app.route('/c/<text>', strict_slashes=False)
 def c(text):
+    """ comment """
+    text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
-@app.route('/python', defaults= {'text': 'is magic'})
-@app.route('/python/', defaults= {'text': 'is magic'})
-@app.route('/python/<text>')
-def python(text):
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythonF(text):
+    """ comment """
     text = text.replace('_', ' ')
     return ('Python {}'.format(text))
 
+
 @app.route('/number/<int:n>')
 def number(n):
+    """ comment """
     return '%s is a number' % n
+
 
 @app.route('/number_template/<int:n>')
 def number_template(n):
+    """ comment """
     return (render_template('5-number.html', n=n))
 
 if __name__ == '__main__':
+    """ comment """
     app.run(host='0.0.0.0', debug=False)
