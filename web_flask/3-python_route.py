3a7c044a110f0bce2eee332142260de6fd262f63
Toyugo <2490@holbertonschool.com>
2021-05-02 14:40:52 +0000

    fix

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,37 +1,36 @@
 #!/usr/bin/python3
 """
-    setup 4 routes and start the app
+    setup 3
 """
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """simple route"""
+def index():
+    """ comment """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ /hbnb route """
+    """ comment """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def arg1(text):
-    """ /c route """
-    tmp = text.replace('_', ' ')
-    return 'C {}'.format(tmp)
+def c(text):
+    """ comment """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def arg2(text):
-    """ /python and optional param """
-    tmp = text.replace('_', ' ')
-    return 'Python {}'.format(tmp)
-
+def pythonF(text):
+    """ comment """
+    text = text.replace('_', ' ')
+    return ('Python {}'.format(text))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', debug=False)
