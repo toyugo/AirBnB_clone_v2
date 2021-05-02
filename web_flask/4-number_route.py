238d207b62215794b47b3f3c6e1b791d292e21df
Toyugo <2490@holbertonschool.com>
2021-05-02 14:41:52 +0000

    fix

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,28 +1,41 @@
+#!/usr/bin/python3
+"""
+    setup 3
+"""
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', strict_slashes=False)
 def index():
+    """ comment """
     return 'Hello HBNB!'
 
-@app.route('/hbnb')
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """ comment """
     return 'HBNB'
 
-@app.route('/c/<text>')
+
+@app.route('/c/<text>', strict_slashes=False)
 def c(text):
+    """ comment """
+    text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
-@app.route('/python', defaults= {'text': 'is magic'})
-@app.route('/python/', defaults= {'text': 'is magic'})
-@app.route('/python/<text>')
-def python(text):
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythonF(text):
+    """ comment """
     text = text.replace('_', ' ')
     return ('Python {}'.format(text))
 
+
 @app.route('/number/<int:n>')
 def number(n):
+    """ comment """
     return '%s is a number' % n
 
 
