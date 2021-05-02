63265926699d0c86a2f312e26cbae135a41e3da0
Toyugo <2490@holbertonschool.com>
2021-05-02 14:24:18 +0000

    fix

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,19 +3,20 @@
     setup 2 routes
 """
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def index():
     """simple route"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """simple route"""
     return 'HBNB'
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False)
-    
