43d9d87357d619a2379ba3a60fc33c70c56425a7
Toyugo <2490@holbertonschool.com>
2021-05-02 14:35:20 +0000

    fix

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,24 +1,33 @@
+#!/usr/bin/python3
+"""
+    setup 3
+"""
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+
+@app.route('/', strict_slashes=False)
 def index():
+    """ comment """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """ comment """
     return 'HBNB'
 
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes=False)
 def c(text):
+    """ comment """
+    text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
 
-@app.route('/python', defaults={'text': 'is magic'})
-@app.route('/python/', defaults={'text': 'is magic'})
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 @app.route('/python/<path:text>')
 def python(text):
     text = text.replace('_', ' ')
